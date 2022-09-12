8d20c3c5b5875e4735725ac27d955293aed20340
mreagan-epion <109161704+mreagan-epion@users.noreply.github.com>
2022-09-12 14:34:43 -0500

    more testing

--- ManagedLANGroups.psm1
+++ ManagedLANGroups.psm1
@@ -35,16 +35,16 @@ function Create-ManagedLANGroups {
     $ManagedLANInternetOnlyExists= Get-ADGroup -LDAPFilter "(SAMAccountName=$ManagedLANInternetOnly)"
 
     if ($ManagedLANSecureExists -eq $null) {
-        Write-Host "Creating Secure Managed LAN Group" -ForegroundColor Yellow |
+        Write-Host "Creating Secure Managed LAN Group" -ForegroundColor Yellow
         New-ADGroup `
-        -Name "$ManagedLanSecure" `
-        -SamAccountName "$ManagedLanSecure" `
-        -GroupCategory Security `
-        -GroupScope Global `
-        -DisplayName "$ManagedLanSecure" `
-        -Path "OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName" `
-        -Description "This group is for authorized devices to connect to both the $DomainSuffixName
-        network and internet" |
+            -Name "$ManagedLanSecure" `
+            -SamAccountName "$ManagedLanSecure" `
+            -GroupCategory Security `
+            -GroupScope Global `
+            -DisplayName "$ManagedLanSecure" `
+            -Path "OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName" `
+            -Description "This group is for authorized devices to connect to both the $DomainSuffixName
+            network and internet"
         if ($ManagedLANSecureExists -eq $null) {
             Write-Host "Error Creating the Managed LAN Secure Group. Please check the settings and try again. " -ForegroundColor Red
         } else {
@@ -55,15 +55,15 @@ function Create-ManagedLANGroups {
     } 
 
     if ($ManagedLANInternetOnlyExists -eq $null) {
-            Write-Host "Creating Internet Only Managed LAN Group" |
+            Write-Host "Creating Internet Only Managed LAN Group"
             New-ADGroup `
-            -Name "$ManagedLanInternetOnly" `
-            -SamAccountName "$ManagedLanInternetOnly" `
-            -GroupCategory Security `
-            -GroupScope Global `
-            -DisplayName "$ManagedLanInternetOnly" `
-            -Path "OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName" `
-            -Description "This group is for devices to only access the internet" |
+                -Name "$ManagedLanInternetOnly" `
+                -SamAccountName "$ManagedLanInternetOnly" `
+                -GroupCategory Security `
+                -GroupScope Global `
+                -DisplayName "$ManagedLanInternetOnly" `
+                -Path "OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName" `
+                -Description "This group is for devices to only access the internet" 
             if ($ManagedLANInternetOnlyExists -eq $null) {
                 Write-Host "Error Creating the Managed LAN Internet Only Group. Please check the settings and try again. " -ForegroundColor Red
             } else {
