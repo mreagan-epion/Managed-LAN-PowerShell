6dc60565c2b3260196ce415746f10390f23c32db
mreagan-epion <109161704+mreagan-epion@users.noreply.github.com>
2022-09-15 14:21:49 -0500

    take.. whatever...

--- createManagedLANDevices.psm1
+++ createManagedLANDevices.psm1
@@ -20,7 +20,7 @@ function Import-ManagedLANDevices {
     $PrintersOUPath = "OU=Printers,OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName"
     $ThinClientsOUPath = "OU=Thin Clients,OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName"
     $MiscOUPath = "OU=Misc,OU=Managed LAN,OU=EpiOn,DC=$DomainPrefixName,DC=$DomainSuffixName"
-    $OUPathGroup = @($DesktopOUPath, $PhonesOUPath, $PrintersOUPath, $ThinClientsOUPath, $MiscOUPath)
+    $OUPathList = @($DesktopOUPath, $PhonesOUPath, $PrintersOUPath, $ThinClientsOUPath, $MiscOUPath)
 
     #Default Group Assignments for each device type
     $DesktopGroup = Get-ADGroup "Managed LAN VLAN 1 - Secure" -Properties @("PrimaryGroupToken")
@@ -28,60 +28,61 @@ function Import-ManagedLANDevices {
     $PrinterGroup = Get-ADGroup "Managed LAN VLAN 1 - Secure" -Properties @("PrimaryGroupToken")
     $ThinClientGroup = Get-ADGroup "Managed LAN VLAN 1 - Secure" -Properties @("PrimaryGroupToken")
     $MiscGroup = Get-ADGroup "Managed LAN VLAN 20 - Internet Only" -Properties @("PrimaryGroupToken")
-    $groups = @($DesktopGroup, $PhoneGroup, $PhoneGroup, $ThinClientGroup, $MiscGroup)
+    $groups = @($DesktopGroup, $PhoneGroup, $PrinterGroup, $ThinClientGroup, $MiscGroup)
 
     #Variables for the device creation loop
     $DomainName = (Get-WmiObject Win32_ComputerSystem).Domain
-    $DomainUPN="@$DomainName"
+    $DomainUPN = "@$DomainName"
     #Used to specifiy a specific server for creating accounts. Without this, the script might hit more than one DC and that
     #will generate errors.
-    $DomainServer=(Get-ADDomain).PDCEmulator
-
+    $DomainServer = (Get-ADDomain).PDCEmulator
+    $listIncrement = 0
+    $groupIncrement = 0
+    $pathIncrement = 0
         foreach ($list in $allDeviceLists) {
-            foreach ($device in $list) {
-                foreach ($group in $groups) {
-                    foreach ($path in $OUPathGroup) {
-                        if (Get-ADUser -Filter "sAMAccountName -eq '$device'") {
-                            "Desktop User Account '$device' Exists Already"}
-                        else {
-                        Write-Host "Creating Desktop User '$device'"
-                        New-ADUser `
-                            -Server $DomainServer `
-                            -Name $device `
-                            -Path "$path" `
-                            -UserPrincipalName "$device$DomainUPN" `
-                            -AccountPassword (convertto-securestring "%Ehy7QX#l@CWo$A*5IkO" -AsPlainText -Force) `
-                            -Enabled $true `
-                            -PasswordNeverExpires $true `
-                            -AllowReversiblePasswordEncryption $true 
-                                    
-                        Add-ADGroupMember `
-                            -Server $DomainServer `
-                            -identity "$group" `
-                            -Members $device
-            
-                        Get-ADUser `
-                            -Server $DomainServer `
-                            -identity $device | Set-ADUser `
-                            -Server $DomainServer `
-                            -Replace @{primarygroupid=$group.primarygrouptoken}
-            
-                        Remove-ADGroupMember `
-                            -Server $DomainServer `
-                            -identity "Domain Users" `
-                            -Members "$device" `
-                            -confirm:$false
-            
-                        Set-ADAccountPassword `
-                            -Server $DomainServer `
-                            -Identity $device `
-                            -NewPassword (ConvertTo-SecureString `
-                                -AsPlainText $device `
-                                -Force) `
-                                -Reset `
-                    }
-                }
+            foreach ($device in $list[$listIncrement]) {
+                if (Get-ADUser -Filter "sAMAccountName -eq '$device'") {
+                    "Desktop User Account '$device' Exists Already"}
+                else {
+                Write-Host "Creating Desktop User '$device'"
+                New-ADUser `
+                    -Server $DomainServer `
+                    -Name $device `
+                    -Path "$OUPathList[$pathIncrement]" `
+                    -UserPrincipalName "$device$DomainUPN" `
+                    -AccountPassword (convertto-securestring "%Ehy7QX#l@CWo$A*5IkO" -AsPlainText -Force) `
+                    -Enabled $true `
+                    -PasswordNeverExpires $true `
+                    -AllowReversiblePasswordEncryption $true 
+                            
+                Add-ADGroupMember `
+                    -Server $DomainServer `
+                    -identity "$groups[$groupIncrement]" `
+                    -Members $device
+    
+                Get-ADUser `
+                    -Server $DomainServer `
+                    -identity $device | Set-ADUser `
+                    -Server $DomainServer `
+                    -Replace @{primarygroupid=$groups[$groupIncrement].primarygrouptoken}
+    
+                Remove-ADGroupMember `
+                    -Server $DomainServer `
+                    -identity "Domain Users" `
+                    -Members "$device" `
+                    -confirm:$false
+    
+                Set-ADAccountPassword `
+                    -Server $DomainServer `
+                    -Identity $device `
+                    -NewPassword (ConvertTo-SecureString `
+                        -AsPlainText $device `
+                        -Force) `
+                        -Reset `
             }
         }
+        $listIncrement++
+        $pathIncrement++
+        $groupIncrement++
     }
 }
