7e074a19bb6338e4012d020a63b03781d1a05c7e
Matthew Reagan <mreagan@epion.com>
2022-09-23 10:31:39 -0500

    step 1 updates

--- step1.ps1
+++ step1.ps1
@@ -1,43 +1,43 @@
 
 
 
-#Version 1.3
-#Developed By Corey and Matthew
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+# #Version 1.3
+# #Developed By Corey and Matthew
+# [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+# $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+# [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
-#Name of client/site.  This will pull from all sites of the client unless you specify the exact name of the client site in Unifi
-$Client = Read-Host "What is the UniFi Tenant Name?"
+# #Name of client/site.  This will pull from all sites of the client unless you specify the exact name of the client site in Unifi
+# $Client = Read-Host "What is the UniFi Tenant Name?"
 
-#Prompts for password to connect to Unifi Controller.
-$password = Read-Host -assecurestring "Please enter the admin password for the Unifi Controller: "
-$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+# #Prompts for password to connect to Unifi Controller.
+# $password = Read-Host -assecurestring "Please enter the admin password for the Unifi Controller: "
+# $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
-#Filename of CSV export.
-$ExportFilename = "C:\Temp\TestMAC.csv"
-if (Test-Path $ExportFilename) { Remove-Item $ExportFilename; }
-New-Item $ExportFilename -ItemType "file" -Value "Mac address`r`n"
+# #Filename of CSV export.
+# $ExportFilename = "C:\Temp\TestMAC.csv"
+# if (Test-Path $ExportFilename) { Remove-Item $ExportFilename; }
+# New-Item $ExportFilename -ItemType "file" -Value "Mac address`r`n"
 
-#Unifi Controller IP/port with base API folder.
-$baseURI = "https://10.1.5.16:8443/api"
+# #Unifi Controller IP/port with base API folder.
+# $baseURI = "https://10.1.5.16:8443/api"
 
-#Login page for Unifi Controller
-$loginURI = "$baseURI/login"
+# #Login page for Unifi Controller
+# $loginURI = "$baseURI/login"
 
-#Credentials to log into the Unifi Controller
-$cred = "`{`"username`":`"epion`",`"password`":`"$password`"`}"
+# #Credentials to log into the Unifi Controller
+# $cred = "`{`"username`":`"epion`",`"password`":`"$password`"`}"
 
-#Connecting to Unifi Controller and creating session
-Invoke-RestMethod -uri $loginURI -Method Post -Body $cred -ContentType "application/json" -SessionVariable session | out-null
+# #Connecting to Unifi Controller and creating session
+# Invoke-RestMethod -uri $loginURI -Method Post -Body $cred -ContentType "application/json" -SessionVariable session | out-null
 
-#Pulling list of sites on Unifi Controller and filtering by Client.
-$response = Invoke-RestMethod -Uri "$baseURI/self/sites" -Method Get  -WebSession $session
-$returnedSites = $response.data | select name,desc | where desc -like "*$Client*" | sort-object desc
+# #Pulling list of sites on Unifi Controller and filtering by Client.
+# $response = Invoke-RestMethod -Uri "$baseURI/self/sites" -Method Get  -WebSession $session
+# $returnedSites = $response.data | select name,desc | where desc -like "*$Client*" | sort-object desc
 
-#Pulling list of client device mac addresses and exporting to a CSV file
-$finalresult = $returnedSites | select name,desc,@{n="devices";e={Invoke-RestMethod -Uri "$baseURI/s/$($_.name)/stat/sta" -Method Post -Body "" -WebSession $session}}
-($finalresult.devices.data | where {$_.is_wired} | select mac | format-table -hidetableheaders | out-string).toupper().trim() | Out-File $ExportFilename -Append -Encoding ASCII
+# #Pulling list of client device mac addresses and exporting to a CSV file
+# $finalresult = $returnedSites | select name,desc,@{n="devices";e={Invoke-RestMethod -Uri "$baseURI/s/$($_.name)/stat/sta" -Method Post -Body "" -WebSession $session}}
+# ($finalresult.devices.data | where {$_.is_wired} | select mac | format-table -hidetableheaders | out-string).toupper().trim() | Out-File $ExportFilename -Append -Encoding ASCII
 #CSV files with MACs that are related to either the Phones, Desktops, or Printers on the UniFi device list. 
 $MyPath=Split-Path $MyInvocation.MyCommand.Path -Parent
 $KnownDesktopMACs=Import-Csv -Path "$MyPath\KnownMACs\KnownMACs_Desktops.csv"
