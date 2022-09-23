9afa95acb9d76a7c3194ae47e3b3a1b828b3c7a8
mreagan-epion <109161704+mreagan-epion@users.noreply.github.com>
2022-09-23 10:56:05 -0500

    testing script updated

--- testingUniFiModule.ps1
+++ testingUniFiModule.ps1
@@ -1,29 +1,29 @@
 
 
 
-# Import-Module .\connectToUniFiController.psm1
+Import-Module .\connectToUniFiController.psm1
 
-# Export-Devices -URI 10.1.5.16
+Export-Devices -URI 10.1.5.16
 
-function test {
-    param (
-        # OptionalParameters
-    )
-    $var = @("Hello World", "Holo World?")
+# function test {
+#     param (
+#         # OptionalParameters
+#     )
+#     $var = @("Hello World", "Holo World?")
 
-    return $var
-}
+#     return $var
+# }
 
-function test2 {
-    param (
-        # OptionalParameters
-    )
-    $var2 = test
+# function test2 {
+#     param (
+#         # OptionalParameters
+#     )
+#     $var2 = test
 
-    # Write-Host $var2
-    return $var2
+#     # Write-Host $var2
+#     return $var2
     
-}
+# }
 
-$test3 = test2
-Write-Host "This is the value of the second value: $($test3[1])"
+# $test3 = test2
+# Write-Host "This is the value of the second value: $($test3[1])"
