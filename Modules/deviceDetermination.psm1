146d3aca3e98f72ef8b3bce809ccce17d33f3a0a
Matthew Reagan <mreagan@epion.com>
2022-10-26 14:47:12 -0500

    brand updates

--- Modules/deviceDetermination.psm1
+++ Modules/deviceDetermination.psm1
@@ -7,9 +7,9 @@ function Get-DeviceType {
         [string] $macAddress
     )
 
-    $commonDesktopBrands = @("Dell", "Dell Inc.", "Elitegro", "Micro-St", "Ce Link Limited", "AsustekC", "ASRock Incorporation", "Plugable Technologies", "Winstars Technology Ltd", "IEEE Registration Authority", "ASUSTek COMPUTER INC.")
-    $commonPhoneBrands = @("Esi", "Zultys", "XiamenYe", "Xiamen Yealink Network Technology Co.,Ltd", "Sensapho", "")
-    $commonPrinterBrands = @("KYOCERA Display Corporation", "HewlettP", "Hewlett Packard", "Lexmark International, Inc.", "KonicaMi", "Brother Industries, LTD.")
+    $commonDesktopBrands = @("Dell", "Dell Inc.", "Elitegro", "Micro-St", "Ce Link Limited", "AsustekC", "ASRock Incorporation", "Plugable Technologies", "Winstars Technology Ltd", "IEEE Registration Authority", "ASUSTek COMPUTER INC.", "Broadcom Limited")
+    $commonPhoneBrands = @("Esi", "Zultys", "XiamenYe", "Xiamen Yealink Network Technology Co.,Ltd", "Sensapho", "Polycom")
+    $commonPrinterBrands = @("KYOCERA Display Corporation", "HewlettP", "Hewlett Packard", "Lexmark International, Inc.", "KonicaMi", "Brother Industries, LTD.", "Canon, Inc.")
     $commonThinClientBrands = @("Raspberry Pi Foundation", "Raspberry Pi Trading Ltd")
     
 
